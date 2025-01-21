bcb81ebdd538894fc0917fd61f06655ccdca1fcc
dmitryhatsayuk <155708514+dmitryhatsayuk@users.noreply.github.com>
2025-01-22 02:24:06 +0700

    Initial commit

---
+++ src/main/java/ru/netology/moneytransfer2/Controller.java
@@ -0,0 +1,4 @@
+package ru.netology.moneytransfer2;
+
+public class Controller {
+}
---
+++ src/main/java/ru/netology/moneytransfer2/MoneyTransfer2Application.java
@@ -0,0 +1,13 @@
+package ru.netology.moneytransfer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransfer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransfer2Application.class, args);
+	}
+
+}
